90bc3113c55fe10747edbe9b863bbdeab941fcea
integramagna <hi@integramagna.com>
2025-07-28 13:27:57 +0530

    clarity

--- app/layout.jsx
+++ app/layout.jsx
@@ -98,6 +98,19 @@ export default function RootLayout({ children }) {
             `,
           }}
         />
+        <Script
+  id="clarity-script"
+  strategy="afterInteractive"
+  dangerouslySetInnerHTML={{
+    __html: `
+        (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "slvab8trwz");
+    `,
+  }}
+/>
 
         {/* Transition Overlay */}
         <div id="transition-overlay" className={`transitionOverlay ${isInitialLoad ? "" : "hidden"}`}

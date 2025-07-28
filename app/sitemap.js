dc70a282bfc96ace3ba4456c2d1668de861c0bdb
integramagna <hi@integramagna.com>
2025-07-28 11:39:43 +0530

    Update sitemap.js

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,77 +1,99 @@
 export default function sitemap() {
-    return [
-      {
-        url: 'https://integramagna.com',
-        lastModified: new Date(),
-      },
-      {
-        url: 'https://integramagna.com/work',
-        lastModified: new Date(),
-      },
-      {
-        url: 'https://integramagna.com/services',
-        lastModified: new Date(),
-      },
-      {
-        url: 'https://integramagna.com/thinking',
-        lastModified: new Date(),
-      },
-      {
-        url: 'https://integramagna.com/career',
-        lastModified: new Date(),
-      },
-      {
-        url: 'https://integramagna.com/about',
-        lastModified: new Date(),
-      },
-      {
-        url: 'https://integramagna.com/contact',
-        lastModified: new Date(),
-      },
-      {
-        url: 'https://integramagna.com/privacy-policy',
-        lastModified: new Date(),
-      },
-      {
-        url: 'https://integramagna.com/work/boat',
-        lastModified: new Date(),
-      },
-      {
-        url: 'https://integramagna.com/work/royal-legacy',
-        lastModified: new Date(),
-      },
-      {
-        url: 'https://integramagna.com/work/decathlon',
-        lastModified: new Date(),
-      },
-      {
-        url: 'https://integramagna.com/work/adnd',
-        lastModified: new Date(),
-      },
-      {
-        url: 'https://integramagna.com/work/prestige-university',
-        lastModified: new Date(),
-      },
-      {
-        url: 'https://integramagna.com/work/motivata',
-        lastModified: new Date(),
-      },
-      {
-        url: 'https://integramagna.com/work/neelam-foresteria',
-        lastModified: new Date(),
-      },
-      {
-        url: 'https://integramagna.com/work/tilx',
-        lastModified: new Date(),
-      },
-      {
-        url: 'https://integramagna.com/work/taskey',
-        lastModified: new Date(),
-      },
-      {
-        url: 'https://integramagna.com/work/hari-keshri',
-        lastModified: new Date(),
-      },
-    ];
-  }
-  
+  return [
+    {
+      url: 'https://www.integramagna.com',
+      lastModified: new Date(),
+      priority: 1.0,
+    },
+    {
+      url: 'https://www.integramagna.com/work',
+      lastModified: new Date(),
+      priority: 0.9,
+    },
+    {
+      url: 'https://www.integramagna.com/services',
+      lastModified: new Date(),
+      priority: 0.9,
+    },
+    {
+      url: 'https://www.integramagna.com/thinking',
+      lastModified: new Date(),
+      priority: 0.9,
+    },
+    {
+      url: 'https://www.integramagna.com/career',
+      lastModified: new Date(),
+      priority: 0.8,
+    },
+    {
+      url: 'https://www.integramagna.com/about',
+      lastModified: new Date(),
+      priority: 0.9,
+    },
+    {
+      url: 'https://www.integramagna.com/contact',
+      lastModified: new Date(),
+      priority: 0.9,
+    },
+    {
+      url: 'https://www.integramagna.com/privacy-policy',
+      lastModified: new Date(),
+      priority: 0.3,
+    },
+    {
+      url: 'https://www.integramagna.com/work/boat',
+      lastModified: new Date(),
+      priority: 0.7,
+    },
+    {
+      url: 'https://www.integramagna.com/work/royal-legacy',
+      lastModified: new Date(),
+      priority: 0.7,
+    },
+    {
+      url: 'https://www.integramagna.com/work/decathlon',
+      lastModified: new Date(),
+      priority: 0.7,
+    },
+    {
+      url: 'https://www.integramagna.com/work/adnd',
+      lastModified: new Date(),
+      priority: 0.7,
+    },
+    {
+      url: 'https://www.integramagna.com/work/prestige-university',
+      lastModified: new Date(),
+      priority: 0.7,
+    },
+    {
+      url: 'https://www.integramagna.com/work/motivata',
+      lastModified: new Date(),
+      priority: 0.7,
+    },
+    {
+      url: 'https://www.integramagna.com/work/neelam-foresteria',
+      lastModified: new Date(),
+      priority: 0.7,
+    },
+    {
+      url: 'https://www.integramagna.com/work/tilx',
+      lastModified: new Date(),
+      priority: 0.7,
+    },
+    {
+      url: 'https://www.integramagna.com/work/taskey',
+      lastModified: new Date(),
+      priority: 0.7,
+    },
+    {
+      url: 'https://www.integramagna.com/work/hari-keshri',
+      lastModified: new Date(),
+      priority: 0.7,
+    },
+    {
+      url: 'https://www.integramagna.com/work/marriott',
+      lastModified: new Date(),
+      priority: 0.7,
+    },
+  ];
+}
